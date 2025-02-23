3ece598a193973393500da93957a612bbad3a5e4
nluttenberger <60539840+nluttenberger@users.noreply.github.com>
2025-02-23 14:43:14 +0100

    new js files

--- js/getKey.js
+++ js/getKey.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function install () {
+function getKey () {
   let pwd = document.getElementById('pwd').value;
   if (pwd.length<12 || pwd.length>12) {
     alert('Der Freischaltcode hat 12 Zeichen.')
@@ -13,7 +13,6 @@ function install () {
   for (let j=1;j<hash32arr.length;j++) {
     hash32arr[j] = parseInt(hash.substr(j*8,8),16);
   }
-  console.log (hash32arr);
   let mask = new Uint32Array (10);
   mask[0] = 1734897759;
   mask[1] = 2548162100;
@@ -27,17 +26,14 @@ function install () {
   mask[9] = 744962278;
   let key = new Uint32Array (10);
   let apiKey = '';
-  let x = '';
   let hex = '';
   for (let k=0;k<key.length;k++) {
     key[k] = hash32arr[k] ^ mask[k];
     hex = key[k].toString(16);
-    console.log (hex);
     for (let n = 0; n < 8; n=n+2) {
-      apiKey += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+      apiKey += String.fromCharCode(parseInt(hex.substring(n, n+2), 16));
     }
   }
-  //console.log (apiKey);
   window.localStorage.setItem('apiKey', `token ${apiKey}`);
   alert ('Bingo!');
 }
