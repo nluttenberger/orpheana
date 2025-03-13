ece635c01fd3250db55af6b0832ef4baed72f68d
nluttenberger <60539840+nluttenberger@users.noreply.github.com>
2025-03-13 17:59:56 +0100

    Update collect.js

--- js/collect.js
+++ js/collect.js
@@ -850,7 +850,7 @@ function collect () {
                 <input type="text" name="GND" value="${vAuthorGND}"></input>`;
                 console.log(vSubject);
                 console.log(vOccasion);
-                innerContainer.querySelector("select[name='subject']").value = 'anders';
+                console.log(innerContainer.querySelector("select[name='subject']").value);
                 innerContainer.querySelector("select[name='occasion']").value = vOccasion;   
                 const paragraphs = text.querySelectorAll("paragraph");
                 paragraphs.forEach(paragraph => {
