e4d34e235fa5854508133dffd82df6574d471217
nluttenberger <60539840+nluttenberger@users.noreply.github.com>
2025-04-15 17:20:14 +0200

    Update register.js

--- js/register.js
+++ js/register.js
@@ -34,6 +34,7 @@ function register() {
         '<?xml version="1.0" encoding="UTF-8"?> \n' +
         '<orph:orph xmlns:orph="http://orpheana.de/ns/orph" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://orpheana.de/ns/orph ../../../../03%20tools/orphSchema.xsd"></orph:orph>', 'text/xml'); 
     const orph = xml.querySelector("orph");
+    console.log(orph);
 
     // short section
     const short = xml.createElement("orph:short");
